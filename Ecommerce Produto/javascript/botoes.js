0224a35efdcf2a55747c9ed5dccd7bff5efc19de
Elaine Santos <101003900+elainesantos-in@users.noreply.github.com>
2023-08-04 18:13:56 -0300

    exercicio

--- Ecommerce Produto/javascript/botoes.js
+++ Ecommerce Produto/javascript/botoes.js
@@ -1,19 +1,31 @@
-let subtrairCarrinho = Number(document.querySelector('.botao--subtracao'));
-let quantidadeCarrinho = Number(document.querySelector('.quantidade'));
-let adicaoCarrinho = Number(document.querySelector('.botao--adicao'));
+let subtrair = document.querySelector('.botao--subtracao');
+let quantidade = document.querySelector('.quantidade');
+let adicao = document.querySelector('.botao--adicao');
+let alerta = document.querySelector('.alerta')
+console.log('aqui')
+let subtrairCarrinho = Number(subtrair.value);
+let quantidadeCarrinho = Number(quantidade.value);
+let adicaoCarrinho = Number(adicao.value);
+console.log('aqui')
 
-subtrairCarrinho.addEventListener('click', diminuirQuantidade);
 adicaoCarrinho.addEventListener('click', aumentarQuantidade);
+subtrairCarrinho.addEventListener('click', diminuirQuantidade);
 
-
+function aumentarQuantidade() {
+    if (quantidade >= 5){
+       alerta.style.display = 'block';
+   } else {
+       quantidadeCarrinho = quantidadeCarrinho + 1
+   }
+   }
 function diminuirQuantidade() {
     if (quantidadeCarrinho > 0 ){
         quantidadeCarrinho =  quantidadeCarrinho - 1
-        console.log(aqui)
+        console.log('aqui')
+    }else if(quantidade > 5){
+        quantidade = 5
+        alerta.style.display = 'block';
     }
 
 }
 
-function aumentarQuantidade() {
-    quantidadeCarrinho = quantidadeCarrinho + 1
-}
