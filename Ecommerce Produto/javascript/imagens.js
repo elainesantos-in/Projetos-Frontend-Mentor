8352d11be6294cba937abcd6e2f98fb6a9490cea
Elaine Santos <101003900+elainesantos-in@users.noreply.github.com>
2023-08-08 17:20:49 -0300

    Create imagens.js

---
+++ Ecommerce Produto/javascript/imagens.js
@@ -0,0 +1,15 @@
+var miniImagens = document.querySelector('.imagem-thumbnail');console.log('aqui')
+var carroselImagens = document.querySelector('.imagens--grande');console.log('aqui')
+
+
+miniImagens.addEventListener('click', abrirImagens); console.log('aqui')
+
+
+console.log('aqui')
+
+function abrirImagens() {
+    console.log('aqui')
+    carroselImagens.style.display ='block'
+}
+
+
