04744b9e6fa28639f65bdbd64260140197c75f03
Elaine Santos <101003900+elainesantos-in@users.noreply.github.com>
2023-08-12 15:30:46 -0300

    exercicio

--- Ecommerce Produto/javascript/imagens.js
+++ Ecommerce Produto/javascript/imagens.js
@@ -4,12 +4,18 @@ var carroselImagens = document.querySelector('.imagens--grande');console.log('aq
 
 miniImagens.addEventListener('click', abrirImagens); console.log('aqui')
 
+function abrirImagens() {
+
+    for (let i = 0; i < miniImagens.length; i++) {
+    const img = document.createElement("img");
+    img.src = miniImagens[i];
+    }
+carroselImagens.appendChild(img);
+carroselImagens.style.display ='block';
+}
 
 console.log('aqui')
 
-function abrirImagens() {
-    console.log('aqui')
-    carroselImagens.style.display ='block'
-}
+
 
 
