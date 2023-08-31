8e1c050c0c77b2c4ab8a078aab584eb6b27f1e85
Elaine Santos <101003900+elainesantos-in@users.noreply.github.com>
2023-08-30 21:06:45 -0300

    estudos

--- Calculator de Gorjeta/codigo.js
+++ Calculator de Gorjeta/codigo.js
@@ -1,22 +1,54 @@
-var valorTotal = document.getElementsByClassName('entrada--valor');
+var valorTotal = document.querySelector('.entrada--valor');
 var porcentagens = document.querySelectorAll('.valor--porcentagem');
-var quantidadedePessoas = document.getElementsByClassName('entrada--pessoas');
+var quantidadedePessoas = document.querySelector('.entrada--pessoas');
+var valorTotalGorjeta = document.querySelector('.resultadoTotal--gorjeta');
+var valorTotalGorjetaporPessoa = document.querySelector('.resultadoporPessoa--gorjeta');
 
-porcentagens.forEach((porcent) => {
-    porcent.addEventListener('click', () => {
-        porcent.classList.add('valorclicado');
-    });
-});
-  
-  /*// Função para destacar a nota selecionada
-  function selecionandoPorcentagem(index) {
-    // Remover a classe "nota-selecionada" de todas as notas
-    notas.forEach(nota => {
-      nota.classList.remove('nota-selecionada');
-    });
-  
-    // Adicionar a classe "nota-selecionada" à nota clicada
-    notas[index].classList.add('nota-selecionada');
-  }
+//valorTotal.addEventListener('keyup', valorReal);
+//quantidadedePessoas.quantidadedePessoas
 
-  */
+/*function valorReal(){
+    var valor = parseFloat(valorTotal.value); // Converte o valor para número
+
+    // Atualiza os resultados
+    resultadoporPessoa.innerText = valor.toFixed(2);
+    resultadoTotal.innerText = valor.toFixed(2);
+}
+*/
+quantidadedePessoas.addEventListener('keyup', porPessoas)
+
+porcentagens.forEach(valor => {
+    valor.addEventListener('click', () => {
+        porcentagens.forEach(valorUm => {
+            valorUm.classList.remove('botaoPorcentagemClicado');
+        });
+
+        valor.classList.add('botaoPorcentagemClicado');
+        
+        var porcentagemSelecionada = parseFloat(valor.innerText) / 100; // Converte a porcentagem para um número decimal
+
+        var gorjetaTotal= parseFloat(valorTotal.value) * porcentagemSelecionada;
+        
+        // Atualiza os resultados
+        valorTotalGorjeta.innerText = gorjetaTotal.toFixed(2);
+        valorTotalGorjetaporPessoa.innerText = gorjetaTotal / parseFloat(quantidadedePessoas)
+
+    });
+});  
+
+function porPessoas()
+
+    porcentagens.forEach(valorporcentagem =>{
+        var porcentagemSelecionada = parseFloat(valorporcentagem.innerText) / 100
+        var gorjetaTotal= parseFloat(valorTotal.value) * porcentagemSelecionada;
+        
+        valorTotalGorjetaporPessoa.innerText = gorjetaTotal / parseFloat(quantidadedePessoas)
+
+
+}
+    porPessoas(valorTotal.value,)
+)
+
+
+
+
