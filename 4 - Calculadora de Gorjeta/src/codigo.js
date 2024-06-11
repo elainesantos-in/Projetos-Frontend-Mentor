4d8533b69f8800a539b479277f3a2d89f8880d39
Elaine Santos <101003900+elainesantos-in@users.noreply.github.com>
2024-06-11 19:29:33 -0300

    exercicio

--- 4 - Calculadora de Gorjeta/src/codigo.js
+++ 4 - Calculadora de Gorjeta/src/codigo.js
@@ -18,12 +18,13 @@ document.getElementById("input--custom").addEventListener("input", function() {
 
 // Botões de porcentagem fixa
 var botoesPorcentagem = document.querySelectorAll("button[id^='botao']");
+
 botoesPorcentagem.forEach(function(botao) {
     botao.addEventListener("click", function() {
         var porcentagemFixa = valoresPorcentagem[botao.id];
         var inputCustom = parseFloat(document.getElementById("input--custom").value);
         var porcentagemFinal = porcentagemFixa + (inputCustom || 0);
-        console.log("botão",porcentagemFinal)
+        console.log("botão", porcentagemFinal)
         calcularPorcentagem(porcentagemFinal);
     });
 });
@@ -48,14 +49,42 @@ function calcularPorcentagem(porcentagem) {
         return;
     }
     
-    if(somaPorcentagem ==0 || inputPessoas == 0){
+    if(somaPorcentagem == 0 || inputPessoas == 0){
         return;
     }else{
         var valorGorjeta = (inputDinheiro * (somaPorcentagem ) / 100) / inputPessoas;
         var valorTotal = (inputDinheiro + (inputDinheiro * (somaPorcentagem ) / 100)) / inputPessoas;
 
-        console.log("gosgeta",valorGorjeta)
+        console.log("gorgeta",valorGorjeta)
         document.getElementById("valor--gorjeta").innerText = valorGorjeta.toFixed(2);
         document.getElementById("valor--total").innerText = valorTotal.toFixed(2);
     }
+}
+
+const botaoReset = document.getElementById('reset')
+
+botaoReset.addEventListener("click", limpar);
+
+function limpar(){
+    console.log('entrou')
+    const inputDinheiro = document.getElementById("input--dinheiro");
+    const inputPessoas = document.getElementById("input--pessoas");
+    const inputCustom= document.getElementById("input--custom");
+    const valorGorjeta = document.getElementById("valor--gorjeta");
+    const valorTotal = document.getElementById("valor--total");
+    const avisoPessoas = document.getElementById("aviso-pessoas");
+
+    inputDinheiro.value = ''
+    inputPessoas.value = ''
+    inputCustom.value = ''; // Define como vazio
+
+    // Redefinir as áreas de gorjeta e total
+    valorGorjeta.innerText = "0.00"; // Valor padrão
+    valorTotal.innerText = "0.00"; // Valor padrão
+    
+    // Remover estilos de erro
+    inputDinheiro.classList.remove("campo-invalido");
+    inputPessoas.classList.remove("campo-invalido");
+    avisoPessoas.style.display = 'none'
+    
 }
